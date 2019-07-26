7d3645ec7812c588351c8765364ddfc0c7e809ee
Keith Wong <keith@keiths-macbook-pro.local>
2019-07-26 17:06:49 +0700

    Initial Commit

---
+++ FinalMiniProject(@WhatApp)Tests/FinalMiniProject__WhatApp_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FinalMiniProject__WhatApp_Tests.swift
+//  FinalMiniProject(@WhatApp)Tests
+//
+//  Created by Keith Wong on 7/26/19.
+//  Copyright © 2019 Keith Wong. All rights reserved.
+//
+
+import XCTest
+@testable import FinalMiniProject__WhatApp_
+
+class FinalMiniProject__WhatApp_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
